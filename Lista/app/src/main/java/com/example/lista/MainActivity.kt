3ea4387933a2510ac0326f1658647d3754597a8d
Neifer <52834823+AmineNeifer@users.noreply.github.com>
2021-12-19 22:14:30 +0100

    main activity with button that leads to homePage

--- Lista/app/src/main/java/com/example/lista/MainActivity.kt
+++ Lista/app/src/main/java/com/example/lista/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.lista
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var name : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        name = findViewById(com.example.lista.R.id.nameText)
+    }
+
+    fun connect(v: View) {
+        val intent = Intent(this,HomePage::class.java).apply {
+            putExtra("name", name.text.toString())
+        }
+
+        startActivity(intent)
+
     }
 }
