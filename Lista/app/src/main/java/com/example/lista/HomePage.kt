f5bd672e396206e08134990e3c73af5d83cbfd67
Neifer <52834823+AmineNeifer@users.noreply.github.com>
2021-12-20 18:32:34 +0100

    intent succeeded

--- Lista/app/src/main/java/com/example/lista/HomePage.kt
+++ Lista/app/src/main/java/com/example/lista/HomePage.kt
@@ -2,10 +2,21 @@ package com.example.lista
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+
+        // Get the Intent that started this activity and extract the string
+        val message = intent.getStringExtra("name")
+
+        // Capture the layout's TextView and set the string as its text
+        val textView = findViewById<TextView>(R.id.welcomeText2).apply {
+            text = message
+        }
+
     }
 }
