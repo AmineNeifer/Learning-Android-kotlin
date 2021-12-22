59e6e06be13f4f25be2c7fcb9e6b775466b7c442
Neifer <52834823+AmineNeifer@users.noreply.github.com>
2021-12-22 22:04:17 +0100

    add sqlite insertion but still needs debugging

--- Lista/app/src/main/java/com/example/lista/BucketListActivity.kt
+++ Lista/app/src/main/java/com/example/lista/BucketListActivity.kt
@@ -1,14 +1,31 @@
 package com.example.lista
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class BucketListActivity : AppCompatActivity() {
+    private lateinit var actName : TextView
+    private lateinit var actDesc : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bucket_list)
-        val sharedPreferences = getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
+//        val sharedPreferences = getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
 
+    }
+    fun insertion() {
+        actName = findViewById(R.id.activityName)
+        actDesc = findViewById(R.id.activityDesc)
+        if (actName.text.toString().isNotEmpty() && actDesc.text.toString().isNotEmpty())  {
+            val item = BucketList(actName.text.toString(), actDesc.text.toString())
+            Toast.makeText(this, "haya 1", Toast.LENGTH_SHORT).show()
+            val db = DataBaseHandler(this)
+            Toast.makeText(this, "haya 2", Toast.LENGTH_SHORT).show()
+            db.insertData(item)
+        } else {
+            Toast.makeText(this, "Please fill in both text boxes", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- Lista/app/src/main/java/com/example/lista/DataBaseHandler.kt
+++ Lista/app/src/main/java/com/example/lista/DataBaseHandler.kt
@@ -1,9 +1,44 @@
 package com.example.lista
 
-class DataBaseHandler {
-    val DATABASE_NAME = "BucketDB"
-    val TABLE_NAME = "ta nik ro7y"
-    val COL_ID = "id"
-    val COL_ACT = "activity"
-    val COL_DESC = "description"
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+
+val DATABASE_NAME = "BucketDB"
+val COL_ID = "id"
+val COL_ACT = "activity"
+val COL_DESC = "description"
+
+class DataBaseHandler(var context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {
+//    val sharedPreferences = context.getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
+//    val TABLE_NAME = sharedPreferences.getString("name", null)
+    private val TABLE_NAME = "random"
+    override fun onCreate(db: SQLiteDatabase?) {
+        val createTable = "CREATE TABLE " + TABLE_NAME + " (" +
+                COL_ID + "INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COL_ACT + "VARCHAR(256)," +
+                COL_DESC + "MEDIUMTEXT"
+        db?.execSQL(createTable)
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+    
+    fun insertData(item : BucketList) {
+        val db = this.writableDatabase
+        val cv = ContentValues()
+        cv.put(COL_ACT, item.activity)
+        cv.put(COL_DESC, item.desc)
+        val result = db.insert(TABLE_NAME, null, cv)
+        if (result == (-1).toLong())
+            Toast.makeText(context, "FAILED", Toast.LENGTH_SHORT).show()
+        else
+            Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
+
+
+    }
+
 }
